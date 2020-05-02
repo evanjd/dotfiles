d1f1d21e088d05904ec33965d77af9bdce9582fa
Evan Bruhn <evanjd@Mac-mini.home>
2020-05-02 13:42:14 +1000

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,40 @@
+# Import work-specific functions, aliases, etc
+source ~/.config/fish/work.fish
+
+# Configure Jump
+status --is-interactive; and source (jump shell fish | psub)
+
+# Set the emoji width for iTerm
+set -g fish_emoji_width 2
+
+# Hide the fish greeting
+set fish_greeting ""
+
+# Use legacy fzf keybindings
+set -g FZF_LEGACY_KEYBINDINGS 1
+
+# Fish syntax highlighting
+set -g fish_color_autosuggestion '555'  'brblack'
+set -g fish_color_cancel -r
+set -g fish_color_command --bold
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end brmagenta
+set -g fish_color_error brred
+set -g fish_color_escape 'bryellow'  '--bold'
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator bryellow
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection brblue
+set -g fish_color_search_match 'bryellow'  '--background=brblack'
+set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+
+# Install Starship
+starship init fish | source
